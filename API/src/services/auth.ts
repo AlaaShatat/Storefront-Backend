4bc1e12fbd5820b4574be40d9e71cce2d1376036
Alaa Shatat <alaashabaan102210@gmail.com>
2023-02-13 01:25:06 +0200

    clean the code

--- API/src/services/auth.ts
+++ API/src/services/auth.ts
@@ -18,25 +18,18 @@ const isAuth = (req: express.Request, res: express.Response, next: Function) =>{
     // profile is the requested 
     // so we need to match the signed in with the requested if they are the same 
     const flag: boolean = (req as CustomRequest).auth && Number(req.params.userId) == (req as CustomRequest).auth.id;
-    console.log(flag);
-    console.log("requested " + req.params.userId);
-    console.log("signed " + (req as CustomRequest).auth.id);
     if(!flag){
         return res.status(403).json({
             error: "Access denied"
         });
     }
-
     next();
-
 };
 const isAdmin = async (req: express.Request, res: express.Response, next: Function) =>{
     const allusers = new userStorage();
     const user: User = await allusers.show(Number((req as CustomRequest).auth.id))
-    console.log("in admin", (req as CustomRequest).auth);
     if (user.isadmin == 0){
         // it should be auth as auth is the user signed in  
-        console.log("not admin")
         return res.status(403).json({
             error : "Admin resource! Access denied"
         });
