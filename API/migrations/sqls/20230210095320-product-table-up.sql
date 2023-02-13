17b4e571cd6b39fbbdf1d0af2d406434c6fedf3d
Alaa Shatat <alaashabaan102210@gmail.com>
2023-02-13 12:13:01 +0200

    add products table migration

---
+++ API/migrations/sqls/20230210095320-product-table-up.sql
@@ -0,0 +1,6 @@
+/* Replace with your SQL commands */
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    pName VARCHAR(50),
+    price DECIMAL
+);
--- API/migrations/sqls/20230210095329-alter-users-table-down.sql
+++
@@ -1,6 +0,0 @@
-/* Replace with your SQL commands */
-ALTER TABLE users DROP COLUMN  email;
-
-ALTER TABLE users DROP COLUMN  isAdmin;
-
-ALTER TABLE users DROP CONSTRAINT validate_email;
